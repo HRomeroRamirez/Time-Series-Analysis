ba9cc3412e133ff9892aafb48480663b8fe45cd1
Héctor Romero-Ramírez <116237317+HRomeroRamirez@users.noreply.github.com>
2025-06-05 16:32:45 -0400

    Add files via upload

---
+++ Local Projections/Scripts/00.Setup.R
@@ -0,0 +1,11 @@
+# Filepaths ---------------------------------------------------------------
+workingCopiesFolder <- file.path("/Users", Sys.getenv("USER"),
+                                 "Desktop/Working Copies")
+projectFolder <- file.path(workingCopiesFolder, "Time Series Analysis/Local Projections")
+
+inputsFolder <- file.path(projectFolder, "Inputs")
+outputsFolder <- file.path(projectFolder, "Outputs")
+
+# Version -----------------------------------------------------------------
+
+suffix <- "2025.01.02"
